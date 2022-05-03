8a734142df857226e4fa31ab48867d6d02f9c495
Carlos Gabriel <carlosgmr2304@gmail.com>
2022-05-03 13:48:55 -0300

    Mapeando as minhas entitidades com JPA

--- src/main/java/com/dio/santander/banklineapi/model/Conta.java
+++ src/main/java/com/dio/santander/banklineapi/model/Conta.java
@@ -1,11 +1,18 @@
 package com.dio.santander.banklineapi.model;
 
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Embeddable
 public class Conta {
+  @Column(name = "conta_numero")
   private Long numero;
+
+  @Column(name = "conta_saldo")
   private Double saldo;
 }
